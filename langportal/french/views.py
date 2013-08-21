e0f5fb8a3f00e0ad3f1a66778f6614f9ba0cd744
goya11 <sjwodonoghue@gmail.com>
2013-08-21 18:42:46 +0100

    Beginning to implement page for testing words

--- langportal/french/views.py
+++ langportal/french/views.py
@@ -24,3 +24,23 @@ def index(request):
     return render(request, 'french/index.html', context)
 
 
+
+
+def wordtest(request):
+
+    #    Get min id and max id and then get a random number between them.
+    #    Retrieve from the dictionary model the entry for that ID.
+    #    Send the retrieved information to the template.
+
+    minval = dictionary.objects.all().aggregate(Min('id'))
+    maxval = dictionary.objects.all().aggregate(Max('id'))
+    idval = randint(minval['id__min'],maxval['id__max'])
+    
+    word_details = dictionary.objects.get(id=idval)
+    #print test_words
+    context = Context({'word_details': word_details})
+    return render(request, 'french/wordtest.html', context)
+
+
+
+
--- langportal/langportal/urls.py
+++ langportal/langportal/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -15,5 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^french/', 'french.views.index', name='index'),
+    url(r'^wordtest/', 'french.views.wordtest', name='wordtest'),
+
 
 )
