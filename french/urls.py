76da8364480186fc52845ca8277d53e04bf49163
goya11 <sjwodonoghue@gmail.com>
2013-08-25 17:09:19 +0100

    first attempt at adding user login functionality

--- french/urls.py
+++ french/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import patterns, url
 from french import views
 
 urlpatterns = patterns('',
-    # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
+    url(r'^login', views.login, name='login'),
     url(r'^wordtest/', views.wordtest, name='wordtest'),
     url(r'^checkanswers/', views.checkanswers, name='checkanswers'),
-
-    
+    url(r'^auth/',  views.auth_view, name='auth'),    
+    url(r'^logout/', views.logout, name='logout'),
+    url(r'^loggedin/', views.loggedin, name='loggedin'),
+    url(r'^invalid/', views.invalid_login, name='invalid_login'),
     # ex: /polls/5/results/
     #url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
--- french/views.py
+++ french/views.py
@@ -5,6 +5,40 @@ from random import randint
 from django.db.models import Max, Min
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+from django.contrib import auth
+from django.core.context_processors import csrf
+
+
+def login(request):
+    c = {}
+    c.update(csrf(request))    
+    return render_to_response('french/login.html', c)
+    
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('french/loggedin.html')
+    else:
+        return HttpResponseRedirect('french/invalid.html')
+        return HttpResponseRedirect(reverse('french:loggedin'))
+    
+def loggedin(request):
+    return render_to_response('french/loggedin.html', 
+                              {'full_name': request.user.username})
+
+def invalid_login(request):
+    return render_to_response('french/nvalid_login.html')
+
+def logout(request):
+    auth.logout(request)
+    return render_to_response('french/logout.html')
+
+
 
 
 def index(request):
